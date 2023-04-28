13003ed4d22195538e5b1ea2bf15e2bc8146d57c
Matheus Canovas <matheuscanovas9@gmail.com>
2023-04-28 00:44:34 -0300

    Implementando getById e getAll

--- src/main/java/br/com/fiap/checkpoint2/api/controllers/ProdutoController.java
+++ src/main/java/br/com/fiap/checkpoint2/api/controllers/ProdutoController.java
@@ -1,17 +1,41 @@
 package br.com.fiap.checkpoint2.api.controllers;
 
+import br.com.fiap.checkpoint2.api.models.Produto;
 import br.com.fiap.checkpoint2.api.services.ProdutoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 import static br.com.fiap.checkpoint2.api.utils.ConstantsUtils.V1_PRODUTOS;
 
+@Slf4j
 @RestController
 @RequestMapping(V1_PRODUTOS)
 @RequiredArgsConstructor
 public class ProdutoController {
 
     private final ProdutoService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> getById(@PathVariable Long id) {
+        log.info("GET | Iniciado | {} | id: {}", V1_PRODUTOS, id);
+        Produto produtoFinded = this.service.getById(id);
+        log.info("GET | Finalizado | {} | id: {}", V1_PRODUTOS, id);
+        return ResponseEntity.ok(produtoFinded);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> getAll() {
+        log.info("GET | Iniciado | {}", V1_PRODUTOS);
+        List<Produto> produtosFinded = this.service.getAll();
+        log.info("GET | Finalizado | {}", V1_PRODUTOS);
+        return ResponseEntity.ok(produtosFinded);
+    }
 
 }
--- src/main/java/br/com/fiap/checkpoint2/api/services/ProdutoService.java
+++ src/main/java/br/com/fiap/checkpoint2/api/services/ProdutoService.java
@@ -1,9 +1,14 @@
 package br.com.fiap.checkpoint2.api.services;
 
+import br.com.fiap.checkpoint2.api.models.Produto;
 import br.com.fiap.checkpoint2.api.repositories.ProdutoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static br.com.fiap.checkpoint2.api.utils.MessagesConstants.PRODUTO_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +16,14 @@ public class ProdutoService {
 
     private final ProdutoRepository repository;
 
+    public Produto getById(Long id) {
+        if (this.repository.findById(id).isEmpty()) {
+            throw new EntityNotFoundException(PRODUTO_NOT_FOUND);
+        }
+        return this.repository.findById(id).get();
+    }
+
+    public List<Produto> getAll() {
+        return this.repository.findAll();
+    }
 }
