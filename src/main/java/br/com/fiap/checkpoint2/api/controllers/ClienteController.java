9f2ddae13aaeddea0ae82382f532faa3169153af
Matheus Canovas <130945123+omtcnvs@users.noreply.github.com>
2023-04-28 23:10:56 -0300

    Snapshot-28042023 (#1)
    * Implementando controller e service com CRUD
    
    * Removendo imports
    
    * Update models
    
    * Ajustando application.yml

--- src/main/java/br/com/fiap/checkpoint2/api/controllers/ClienteController.java
+++ src/main/java/br/com/fiap/checkpoint2/api/controllers/ClienteController.java
@@ -1,18 +1,66 @@
 package br.com.fiap.checkpoint2.api.controllers;
 
+import br.com.fiap.checkpoint2.api.models.Cliente;
+import br.com.fiap.checkpoint2.api.resources.ClienteResource;
 import br.com.fiap.checkpoint2.api.services.ClienteService;
-import br.com.fiap.checkpoint2.api.utils.ConstantsUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static br.com.fiap.checkpoint2.api.utils.ConstantsUtils.V1_CLIENTES;
+import static br.com.fiap.checkpoint2.api.utils.ConstantsUtils.V1_PRODUTOS;
 
+@Slf4j
 @RestController
 @RequestMapping(value = V1_CLIENTES)
 @RequiredArgsConstructor
 public class ClienteController {
 
     private final ClienteService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getById(@PathVariable Long id) {
+        log.info("GET | Iniciado | {} | id: {}", V1_PRODUTOS, id);
+        Cliente clienteFinded = this.service.getById(id);
+        log.info("GET | Finalizado | {} | id: {}", V1_PRODUTOS, id);
+        return ResponseEntity.ok(clienteFinded);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> getAll() {
+        log.info("GET | Iniciado | {}", V1_PRODUTOS);
+        List<Cliente> clientesFinded = this.service.getAll();
+        log.info("GET | Finalizado | {}", V1_PRODUTOS);
+        return ResponseEntity.ok(clientesFinded);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Cliente> save(@RequestBody @Valid ClienteResource resource) {
+        log.info("POST | Iniciado | {} | /save | {}", V1_PRODUTOS, resource);
+        this.service.save(resource);
+        log.info("POST | Finalizado | {} | /save | {}", V1_PRODUTOS, resource);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<Cliente> edit(@RequestBody @Valid ClienteResource resource, @PathVariable Long id) {
+        log.info("PUT | Iniciado | {} | /edit/{} | {}", V1_PRODUTOS, id, resource);
+        this.service.put(resource, id);
+        log.info("PUT | Finalizado | {} | /edit/{} | {}", V1_PRODUTOS, id, resource);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Cliente> delete(@PathVariable Long id) {
+        log.info("DELETE | Iniciado | {} | /delete/{}", V1_PRODUTOS, id);
+        this.service.delete(id);
+        log.info("DELETE | Finalizado | {} | /delete/{}", V1_PRODUTOS, id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/main/java/br/com/fiap/checkpoint2/api/services/ClienteService.java
+++ src/main/java/br/com/fiap/checkpoint2/api/services/ClienteService.java
@@ -1,8 +1,15 @@
 package br.com.fiap.checkpoint2.api.services;
 
+import br.com.fiap.checkpoint2.api.assemblers.ClienteMapper;
+import br.com.fiap.checkpoint2.api.models.Cliente;
 import br.com.fiap.checkpoint2.api.repositories.ClienteRepository;
+import br.com.fiap.checkpoint2.api.resources.ClienteResource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static br.com.fiap.checkpoint2.api.utils.ValidationUtils.validateIfProdutoExists;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +17,33 @@ public class ClienteService {
 
     private final ClienteRepository repository;
 
+    private final ClienteMapper mapper;
+
+    public Cliente getById(Long id) {
+        validateIfProdutoExists(this.repository.findById(id).isEmpty());
+        return this.repository.getReferenceById(id);
+    }
+
+    public List<Cliente> getAll() {
+        return this.repository.findAll();
+    }
+
+    public void save(ClienteResource resource) {
+        Cliente model = mapper.toModel(resource);
+        this.repository.save(model);
+    }
+
+    public void put(ClienteResource resource, Long id) {
+        validateIfProdutoExists(this.repository.findById(id).isEmpty());
+        Cliente model = mapper.toModel(resource);
+        model.setId(id);
+        this.repository.save(model);
+    }
+
+
+    public void delete(Long id) {
+        validateIfProdutoExists(this.repository.findById(id).isEmpty());
+        Cliente modelToDelete = this.repository.getReferenceById(id);
+        this.repository.delete(modelToDelete);
+    }
 }
