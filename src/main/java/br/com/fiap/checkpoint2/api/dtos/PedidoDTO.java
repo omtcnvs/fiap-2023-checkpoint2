1a5764b41bc6dd03fd896e87743ee0239d4b8f82
Matheus Canovas <matheuscanovas9@gmail.com>
2023-04-29 17:57:46 -0300

    Implementando CRUD na controller e service

--- src/main/java/br/com/fiap/checkpoint2/api/dtos/PedidoDTO.java
+++ src/main/java/br/com/fiap/checkpoint2/api/dtos/PedidoDTO.java
@@ -1,5 +1,6 @@
 package br.com.fiap.checkpoint2.api.dtos;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.util.Date;
@@ -7,6 +8,7 @@ import java.util.Date;
 @Builder
 public record PedidoDTO(
 
+        @NotNull(message = "Numero do pedido deve ser informado.")
         Long numeroPedido,
         String codigoCliente,
         Date dataPedido
--- src/main/java/br/com/fiap/checkpoint2/api/services/PedidoService.java
+++ src/main/java/br/com/fiap/checkpoint2/api/services/PedidoService.java
@@ -1,8 +1,17 @@
 package br.com.fiap.checkpoint2.api.services;
 
+import br.com.fiap.checkpoint2.api.assemblers.PedidoMapper;
+import br.com.fiap.checkpoint2.api.dtos.PedidoDTO;
+import br.com.fiap.checkpoint2.api.dtos.ProdutoDTO;
+import br.com.fiap.checkpoint2.api.models.Pedido;
+import br.com.fiap.checkpoint2.api.models.Produto;
 import br.com.fiap.checkpoint2.api.repositories.PedidoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static br.com.fiap.checkpoint2.api.utils.ValidationUtils.validateIfProdutoExists;
 
 @Service
 @RequiredArgsConstructor
@@ -10,7 +19,33 @@ public class PedidoService {
 
     private final PedidoRepository repository;
 
+    private final PedidoMapper mapper;
 
-    
+    public Pedido getById(Long id) {
+        validateIfProdutoExists(this.repository.findById(id).isEmpty());
+        return this.repository.getReferenceById(id);
+    }
+
+    public List<Pedido> getAll() {
+        return this.repository.findAll();
+    }
+
+    public void save(PedidoDTO resource) {
+        Pedido model = mapper.toModel(resource);
+        this.repository.save(model);
+    }
+
+    public void put(PedidoDTO resource, Long id) {
+        validateIfProdutoExists(this.repository.findById(id).isEmpty());
+        Pedido model = mapper.toModel(resource);
+        model.setId(id);
+        this.repository.save(model);
+    }
+
+    public void delete(Long id) {
+        validateIfProdutoExists(this.repository.findById(id).isEmpty());
+        Pedido modelToDelete = this.repository.getReferenceById(id);
+        this.repository.delete(modelToDelete);
+    }
 
 }
